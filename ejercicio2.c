d72e29455a8bc8cf4a8238531be539a5199ce8e9
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-11-05 13:51:07 +0100

    Actualización V2 ejercicio2

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,25 +3,33 @@
 
 int main(){
 char letra = 'a';
-
+int encontrado;
 // Esta fución es la encargada de guardar la lista de las palabras que ya estan definidas, es decir, esta lista esta formada por 8 cadenas [8], las cuales pueden tener 100 caracteres[100].
 char lista_palabras[8][100] = {
 
-"Hola",
-"Señor"
-"Burro",
+"Cama",
+"Perro",
+"Arma",
 "Pedro",
 "Vim",
-"Casa",
+"Ordenador",
 "Silla",
 "Ballena"
 
 };
 	for(int i = 0; i < 8; i++){
-		if(lista_palabras[i] == letra){
-		continue;
 
-			}else printf("Las palabras sin la letra a son: %s\n", lista_palabras[i]);
-	}		
+		for (int j = 0; j < 8; j++){
+			
+			if(lista_palabras[i][j] == letra){
+				
+				break;
+
+			}
+		}
+
+		printf("Las palabras sin la letra a son: %s\n", lista_palabras[i]);
+	}
+
 return EXIT_SUCCESS;
 }
