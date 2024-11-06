928cecb39c58b54d97d96fcf0ad99c986fafa7b7
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-11-06 11:03:46 +0100

    Actuualización V1 practica5_2

--- practica5_2.c
+++ practica5_2.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+struct Estudiantes{
+	double nota_final;
+	int pos;
+};
 
-	printf("")
+float promedio(float array[]){
+	
 
+}
+
+int main(){ 
+	int cantidad;
+	
+	printf("Introduce la cantidad de estudiantes para calcular el promedio: ");
+	scanf("%d", &cantidad);
+	struct Estudiantes persona[cantidad];
+
+	for (int i = 1; i <= cantidad; i++){	
+		printf("Introduce la nota final del estudiante número %d: ", i);
+		scanf("%lf", &persona[i].nota_final);
+	}
+
+
+	return EXIT_SUCCESS;
 }
