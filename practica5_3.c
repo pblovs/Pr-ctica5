c6a82ea37a43c6e77d6771a3fdb85810f6017ce4
pblovs <pablovise04@gmail.com>
2024-11-08 12:51:08 +0100

    p5.3 v2

--- practica5_3.c
+++ practica5_3.c
@@ -12,6 +12,10 @@ int main(){
 	int cant;
 	printf("Introduce el número de productos a gestionar (max. 10): ");
 	scanf("%d", &cant);
+	while (cant > 10){
+		printf("¡MÁXIMO 10!\n");
+		return 0;
+	}
 
 	struct tienda productos[10];
 
@@ -25,6 +29,31 @@ int main(){
 	for (int i=0; i<cant; i++){
 		printf("Hay %d unidades de %s\n", productos[i].stock, productos[i].nombre);
 	}
+	
+	while(1){
+
+		int respuesta;
+		printf("¿Desea reabastecer algún producto?\n");
+		printf("Si = 1, No = 0\n");
+		scanf("%d", &respuesta);
+
+		if (respuesta == 0){
+			break;
+		}
+		else {
+			cant++;
+			printf("Introduce el nombre del nuevo producto: ");
+			scanf("%s", productos[cant].nombre);
+			printf("Stock: ");
+			scanf("%d", &productos[cant].stock);
+		}
+	}
+
+	for (int i=0; i<cant; i++){ 
+		printf("Hay %d unidades de %s\n", productos[i].stock,productos[i].nombre);
+	}
+			
+
 
 	
 }
