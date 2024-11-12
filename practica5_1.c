9b81881cb39466c45faa7194f608d27d7b06edb0
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-11-12 13:37:18 +0100

    Comentarios

--- practica5_1.c
+++ practica5_1.c
@@ -36,7 +36,7 @@ int temperatura_min(float a[10]){//Calcula la temperatura mínima de la semana
 	return imin;
 }
 
-int contador_dias(float a[10]){
+int contador_dias(float a[10]){ //Cuenta los días en los que la temperatura es mayor a la del promedio
 	int contador = 0;
 	for (int i = 0; i < 7; i++){
 		if (a[i] > promedio_semana(a)){
@@ -46,7 +46,7 @@ int contador_dias(float a[10]){
 	return contador;
 }
 
-float listado_dias(struct dias semana[], float prom){
+float listado_dias(struct dias semana[], float prom){ //Cuenta los días en los que la temperatura es menor a a la del promedio
 	printf("Dias con temperaturas por debajo del promedio:\n");
 	for (int i = 0; i < 7; i++){
 		if (semana[i].temperatura < prom){
@@ -57,7 +57,7 @@ float listado_dias(struct dias semana[], float prom){
 
 int main(){
 
-	struct dias semana[7];
+	struct dias semana[7]; //Este struct tiene definido los días de la semana que son 7
 
 	strcpy(semana[0].nombre, "Lunes");
 	strcpy(semana[1].nombre, "Martes");
@@ -67,22 +67,22 @@ int main(){
 	strcpy(semana[5].nombre, "Sábado");
 	strcpy(semana[6].nombre, "Domingo");
 
-	for (int i=0; i<7; i++){
+	for (int i=0; i<7; i++){ //Mete los datos recogidos por el usuario para sacar la temperatura media 
 		printf("Temperatura media del %s: ", semana[i].nombre);
 		scanf(" %f", &semana[i].temperatura);
 	}
 
-	float temperaturas[10];
+	float temperaturas[10]; //Guarda todas las temperaturas dadas por el usuario
 	for (int i=0; i<7; i++){
 		temperaturas[i] = semana[i].temperatura;
 	}
 
 
-	float media = promedio_semana(temperaturas);
+	float media = promedio_semana(temperaturas); //En float media guardamos la temperatura media de la semana
 	printf("La temperatura media de la semana es: %.1fºC\n", media);
-	int imax = temperatura_max(temperaturas);
+	int imax = temperatura_max(temperaturas); //Saca el indice de la temperatura máxima
 	printf("Temperatura máxima: %.1fºC el dia %s\n", temperaturas[imax], semana[imax].nombre);
-	int imin = temperatura_min(temperaturas);
+	int imin = temperatura_min(temperaturas); //Saca el indice de la temperatura minima, si sale dos corresponde al miércoles.
 	printf("Temperatura mínima: %.1fºC el dia %s\n", temperaturas[imin], semana[imin].nombre);
 	int dias = contador_dias(temperaturas);
 	printf("Hay %d dia/s superior/es a la media.\n", dias);
--- practica5_2.c
+++ practica5_2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Nota_aprob 5.0
+#define Nota_aprob 5.0 //Se define que la nota para aprobar es un 5
 
-struct Estudiantes{
+struct Estudiantes{ //Estructura que sigue Estudiantes
 	double nota_final;
 	int pos;
 };
 
-float nota_min(float array[100], int cant){
+float nota_min(float array[100], int cant){ //Calcula la nota miníma de los estudiantes
 
 	float min = array[0];
 	for (int i = 1; i < cant; i++){
@@ -35,7 +35,7 @@ int num_aprobados(float array[100], int cant){ //Calcula el número de estudiant
 	for (int i = 0; i < cant; i++){
 		
 		if (array[i] >= Nota_aprob){
-		aprobados++;
+		aprobados++; //Recoge las notas que sean igual o mayor que 5, y los cuenta para saber cuantos alumnos han aprobado, y eso devuelve
 		}
 	}
 	return aprobados;
@@ -46,7 +46,7 @@ int num_suspensos(float array[100], int cant){ //Calcula el número de estudiant
 	for (int i = 0; i < cant; i++){
 
 		if (array[i] < Nota_aprob){
-		suspensos++;
+		suspensos++; //Recoge las notas que sean menor que 5 y los cuenta para saber cuantas notas menor que 5 hay, y eso es lo que devuelve.
 		}
 	}
 	return suspensos;
@@ -61,7 +61,7 @@ float promedio(float array[100], int cant){ //Calcula el promedio de las notas d
 	return suma /cant;
 }
 
-float notas_promedio(float array[100], int cant, float prom){
+float notas_promedio(float array[100], int cant, float prom){ //Calcula el promedio de las notas
 
 	for (int i = 0; i < cant; i++){
 		if(array[i] > prom){
@@ -74,15 +74,15 @@ float notas_promedio(float array[100], int cant, float prom){
 int main(){ 
 	int cantidad;
 	
-	printf("Introduce la cantidad de estudiantes para calcular el promedio: ");
+	printf("Introduce la cantidad de estudiantes para calcular el promedio: "); //Recoge la cantidad de estudiantes dados por el usuario
 	scanf("%d", &cantidad);
 	struct Estudiantes persona[cantidad];
 
-	for (int i = 0; i < cantidad; i++){	
+	for (int i = 0; i < cantidad; i++){ //Recoge las notas finales de los estudiantes, que el usuario da y los guarda en &persona, que sigue la estructura predefinida de Estudiantes.
 		printf("Introduce la nota final del estudiante número %d: ", i);
 		scanf("%lf", &persona[i].nota_final);
 	}
-	float notas[100];
+	float notas[100]; //Guarda las notas finales en persona.nota_final
 	for (int i =0 ; i < cantidad; i++){
 	
 	notas[i] = persona[i].nota_final;
@@ -90,7 +90,7 @@ int main(){
 
 	printf("El promedio de las notas de los estudiantes es: %.1f\n", promedio(notas, cantidad));
 
-	float media = promedio(notas, cantidad);
+	float media = promedio(notas, cantidad); //Extrae el promedio de las notas, que tiene guardado, para usarlos en la nota por encima de la media.
 
 	printf("El número de estudiantes aprobados es: %d\n", num_aprobados(notas, cantidad));
 	printf("El número de estudiantes suspensos es: %d\n", num_suspensos(notas, cantidad));
